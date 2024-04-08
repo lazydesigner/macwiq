12bad95335cecf4878a960fc0703bf42a2bafa62
deepak <deepakbaradwaj933@gmail.com>
2024-04-09 00:36:00 +0530

    initial commit

---
+++ footer.php
@@ -0,0 +1,74 @@
+<footer>
+        <div class="container">
+            <h2>Our Location</h2>
+
+            <div class="footer-flex">
+                <div class="footer-col">
+                    <div><div class="footer-img">
+                        <img src="./image/hq-india.png" alt="">
+                    </div>
+                    <p>HQ INDIA</p>
+                    <p>123, Sector 11, Noida- 201301,
+                        Delhi - NCR, India</p></div>
+                </div>
+                <div class="footer-col">
+                    <div><div class="footer-img">
+                        <img src="./image/hq-usa.png" alt="">
+                    </div>
+                    <p>US</p>
+                    <p>123, 345, Lorim Ipsum,
+                        Lorim Ipsum</p></div>
+                </div>
+                <div class="footer-col">
+                    <div><div class="footer-img">
+                        <img src="./image/hq-canada copy.png" alt="">
+                    </div>
+                    <p>CANADA</p>
+                    <p>123, 345, Lorim Ipsum,
+                        Lorim Ipsum</p></div>
+                </div>
+            </div>
+
+            <div class="footer-box">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, unde.</p>
+                <button>Discover more</button>
+            </div>
+
+            <div class="footer-grid">
+                <div class="grid-box">
+                    <div class="brands">
+                        <img src="./image/Macwiq-logo.png" width="100%" height="100%" alt="">
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae distinctio quam obcaecati animi adipisci consequatur.</p>
+                </div>
+                <div class="grid-box">
+                    <strong>Quick Links</strong>
+                    <ul>
+                        <li>Abour Us</li>
+                        <li>Meet our team</li>
+                        <li>Blos</li>
+                        <li>Our projects</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div class="grid-box">
+                    <strong>contact</strong>
+                    <ul>
+                        <li>+98 (000)-9850</li>
+                        <li>needhelp@notech.com</li>
+                        <li>60 road, broklyn golden street new york</li>
+                    </ul>
+                </div>
+                <div class="grid-box"></div>
+            </div>
+
+
+
+        </div>
+        <div class="footer-bottom">
+            <div class="d">
+                <p>copywrite &copy; 2023 Macwiq AllRight Reserved</p>
+                <p>Designed by Aobha</p>
+            </div>
+        </div>
+    </footer>
