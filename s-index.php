4fb525366920562414d0639e117d21a8f7aef06f
deepak <deepakbaradwaj933@gmail.com>
2024-04-09 09:48:06 +0530

    a

--- s-index.php
+++ s-index.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <link rel="stylesheet" href="./css/s-index.css">
+    <link rel="stylesheet" href="./css/s-index.css?v=<?=time() ?>">
 </head>
 
 <body>
@@ -711,8 +711,11 @@
             },
             breakpoints: {
         // when window width is >= 650px
-        650: {
-            slidesPerView: 1,
+        300: {
+            slidesPerView: 1
+        },
+        650:{
+            slidesPerView: 2,
             spaceBetween: 10
         }
     }
